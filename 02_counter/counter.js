226bc8558bbfccf04339fff37f8cb1f4286fb069
jay-p08 <soRjdla0709@gmail.com>
2026-03-27 13:12:31 +0900

    React Study

--- 02_counter/counter.js
+++ 02_counter/counter.js
@@ -1,7 +1,24 @@
 const container = document.getElementById('container');
-const button = document.getElementById('button');
+// const plusButton = document.getElementById('plusButton');
+// const minusButton = document.getElementById('minusButton');
 
-button.addEventListener('click', () => {
-    const currentValue = parseInt(container.textContent);
-    container.textContent = currentValue + 1;
-});
+
+// plusButton.addEventListener('click', () => {
+//     const currentValue = parseInt(container.textContent);
+//     container.textContent = currentValue + 1;
+// });
+// plusButton.addEventListener('click', () => {
+//     const currentValue = parseInt(container.textContent);
+//     container.textContent = currentValue + 2;
+// }
+// minusButton.addEventListener('click', () => {
+//     const currentValue = parseInt(container.textContent);
+//     container.textContent = currentValue - 1;
+// });
+
+let count = 0;
+
+function eventButton( value = 1 ) {
+    count += value;
+    container.textContent = count;
+} // eventButton()
